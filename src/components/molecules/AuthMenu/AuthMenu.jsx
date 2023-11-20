1028c777bbf8ab99526eabf934e520bf79950f30
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-20 18:28:06 +0100

    Adding Menu for auth and user

---
+++ src/components/molecules/AuthMenu/AuthMenu.jsx
@@ -0,0 +1,8 @@
+export const AuthMenu = () => {
+  return (
+    <>
+      <div>Register button</div>
+      <div>Login button</div>
+    </>
+  );
+};
---
+++ src/components/molecules/UserMenu/UserMenu.jsx
@@ -0,0 +1,8 @@
+export const UserMenu = () => {
+  return (
+    <>
+      <div>User email</div>
+      <div>Logout button</div>
+    </>
+  );
+};
