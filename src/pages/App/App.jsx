4c7713c18d95eedfc090071cf65d758d53a3dabe
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-20 19:54:59 +0100

    Adding routes to App.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { Home } from 'pages/Home/Home';
+import { Layout } from 'pages/Layout/Layout';
+import { Login } from 'pages/Login/Login';
+import { Register } from 'pages/Register/Register';
+import { Contacts } from 'pages/Contacts/Contacts';
+
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={null} />
-      <Route index element={null} />
-      <Route path="/register" element={null} />
-      <Route path="/login" element={null} />
-      <Route path="/contacts" element={null} />
+      <Route path="/" element={<Layout />} />
+      <Route index element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/contacts" element={<Contacts />} />
     </Routes>
   );
 };
