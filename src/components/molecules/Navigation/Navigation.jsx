4c0f306c57a0ab9e26b6cc58bc6ae06e201a0a36
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-22 21:15:51 +0100

    Creating new Nav component. Connecting Header component to redux.

---
+++ src/components/molecules/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from 'react-redux';
+
+import { selectIsLoggedIn } from 'redux/auth/selectors';
+
+import { Link } from 'react-router-dom';
+
+export const Navigation = () => {
+  let isLoggedIn = useSelector(selectIsLoggedIn);
+  isLoggedIn = true;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      {isLoggedIn && <Link to="/contacts">Contacts</Link>}
+    </div>
+  );
+};
--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,13 +1,18 @@
+import { useSelector } from 'react-redux';
+
+import { selectIsLoggedIn } from 'redux/auth/selectors';
+
 import { AuthMenu } from 'components/molecules/AuthMenu/AuthMenu';
 import { UserMenu } from 'components/molecules/UserMenu/UserMenu';
-import { Link } from 'react-router-dom';
+import { Navigation } from 'components/molecules/Navigation/Navigation';
 
 export const Header = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <>
       <header>
-        <Link to="/">Home</Link>
-        {true ? <AuthMenu /> : <UserMenu />}
+        <Navigation />
+        {isLoggedIn ? <UserMenu /> : <AuthMenu />}
       </header>
     </>
   );
