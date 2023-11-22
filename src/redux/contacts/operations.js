f4fd9a526bec397eca0af4eeb4165764e60159f3
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-22 19:06:18 +0100

    Created operations for contact.

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,54 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const ENDPOINT = process.env.REACT_APP_USER_CONTACTS_ENDPOINT;
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(ENDPOINT);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post(ENDPOINT, { ...contact });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contactID, thunkAPI) => {
+    try {
+      const response = await axios.delete(`${ENDPOINT}/${contactID}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const toggleFavourite = createAsyncThunk(
+  'contacts/toggleFavourite',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.put(`${ENDPOINT}/${contact.id}`, {
+        favourite: !contact.favourite,
+      });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
