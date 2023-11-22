0f21cf0020d922746c89cf91b0663a093e3f4eaf
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-22 19:14:12 +0100

    Created selector for contacts.

---
+++ src/redux/contacts/selectors.js
@@ -0,0 +1,36 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+import { selectFilterName, selectFilterFav } from 'redux/filters/selectors';
+
+export const selectContacts = state => state.contacts.item;
+
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilterFav, selectFilterName],
+  (contacts, filterFav, filterName) => {
+    let visibleContacts;
+    switch (filterFav) {
+      case 'all':
+        visibleContacts = contacts.filter(obj =>
+          obj.name.toLowerCase().includes(statusFilter)
+        );
+        break;
+      case 'favourite':
+        visibleContacts = contacts
+          .filter(obj => obj.favourite)
+          .filter(obj => obj.name.toLowerCase().includes(statusFilter));
+        break;
+      case 'nofavourtie':
+        visibleContacts = contacts
+          .filter(obj => !obj.favourite)
+          .filter(obj => obj.name.toLowerCase().includes(statusFilter));
+        break;
+      default:
+        console.log('Error with selectVisibleContacts');
+        break;
+    }
+    return {
+      items: visibleContacts,
+      inPhonebook: contacts.length,
+    };
+  }
+);
