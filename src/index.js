c0c3370816f452669e21a45853262a2892472f56
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-22 20:30:51 +0100

    Uncommenting some code that are ready now to be used.

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
-// import { store, persistor } from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider> */}
-    {/* <PersistGate loading={null} persistor={null}> */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    {/* </PersistGate> */}
-    {/* </Provider> */}
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
