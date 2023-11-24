3c13143032c92689a2f193e65e8e58b10f5ed523
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-24 06:51:09 +0100

    Adding ContactForm into Contacts page.

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,9 +1,11 @@
+import { ContactForm } from 'components/organisms/ContactForm/ContactForm';
 import { ContactsList } from 'components/organisms/ContactList/ContactsList';
 import { Filters } from 'components/organisms/Filters/Filters';
 
 export const Contacts = () => {
   return (
     <div>
+      <ContactForm />
       <Filters />
       <ContactsList />
     </div>
