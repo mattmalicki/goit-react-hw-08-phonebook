0f45ba19a05719943d90bfcac76ce430647951b4
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-24 06:37:13 +0100

    Adding logout button to component and connecting it to redux.

--- src/components/molecules/UserMenu/UserMenu.jsx
+++ src/components/molecules/UserMenu/UserMenu.jsx
@@ -1,8 +1,21 @@
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+
+import { logout } from 'redux/auth/operations';
+import { selectUser } from 'redux/auth/selectors';
+
 export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <>
-      <div>User email</div>
-      <div>Logout button</div>
+      <div>Welcome {user.name}</div>
+      <button onClick={handleLogout}>Logout</button>
     </>
   );
 };
