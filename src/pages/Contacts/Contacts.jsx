497f7fa4aa4df5172fe262756170e216b03d5995
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-25 15:14:31 +0100

    Connecting Contacts page to redux.

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { fetchContacts } from 'redux/contacts/operations';
+
 import { ContactForm } from 'components/organisms/ContactForm/ContactForm';
 import { ContactsList } from 'components/organisms/ContactList/ContactsList';
 import { Filters } from 'components/organisms/Filters/Filters';
 
 const Contacts = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <div>
       <ContactForm />
