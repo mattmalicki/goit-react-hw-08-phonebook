99bdf136021b01c0975bae2720f79d8ff80937a2
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-25 15:27:16 +0100

    Filter is hiding now when there is no contacts inPhonebook.

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchContacts } from 'redux/contacts/operations';
+import { selectVisibleContacts } from 'redux/contacts/selectors';
 
 import { ContactForm } from 'components/organisms/ContactForm/ContactForm';
 import { ContactsList } from 'components/organisms/ContactList/ContactsList';
@@ -9,13 +10,14 @@ import { Filters } from 'components/organisms/Filters/Filters';
 
 const Contacts = () => {
   const dispatch = useDispatch();
+  const contactsAmount = useSelector(selectVisibleContacts).inPhonebook;
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
   return (
     <div>
       <ContactForm />
-      <Filters />
+      {contactsAmount && <Filters />}
       <ContactsList />
     </div>
   );
