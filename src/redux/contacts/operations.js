b443e13dd68eabe4d86ebf88deb16173cfaee84c
mattmalicki <mateusz.malicki96@gmail.com>
2023-12-04 13:47:43 +0100

    Deleting unused functions.

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -20,8 +20,6 @@ export const addContact = createAsyncThunk(
   async (contact, thunkAPI) => {
     try {
       const response = await axios.post(ENDPOINT, {
-        favourite: false,
-        createdAt: Date.now(),
         ...contact,
       });
       return response.data;
@@ -36,20 +34,6 @@ export const deleteContact = createAsyncThunk(
   async (contactID, thunkAPI) => {
     try {
       const response = await axios.delete(`${ENDPOINT}/${contactID}`);
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
-
-export const toggleFavourite = createAsyncThunk(
-  'contacts/toggleFavourite',
-  async (contact, thunkAPI) => {
-    try {
-      const response = await axios.put(`${ENDPOINT}/${contact.id}`, {
-        favourite: !contact.favourite,
-      });
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
