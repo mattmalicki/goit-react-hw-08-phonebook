a8120e47884b17385763d03d3b2b33dc64d4761b
mattmalicki <mateusz.malicki96@gmail.com>
2023-12-04 14:54:39 +0100

    Deleting console logs.

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -56,7 +56,6 @@ export const refresh = createAsyncThunk('auth/refresh', async (_, thunkAPI) => {
   const persistedToken = state.auth.token;
 
   if (persistedToken === null) {
-    console.log('If is correct ', persistedToken);
     return thunkAPI.rejectWithValue('Unable to fetch user');
   }
 
